72531fb085ce7167803da029c880c5ad2da210ee
chrisrandall <chris.f.randall@gmail.com>
2022-05-16 11:14:17 -0600

    commit tree 8a94e6a6259fe5a9cc9aa84008a707a734e6fb1d

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = "Chris Randall";
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+
+const shortGreeting = "Welcome, " + currentUser.slice(0,1) + "!";
+
+
